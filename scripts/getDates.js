165ef671ffc48ef93e377dec2180d7bc524758b2
nebula682 <malcolmnie@gmail.com>
2024-05-02 11:09:32 +0200

    week 01

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,12 @@
 //toLocaleDateString
 const date = {weekday:'long', day:'numeric',month:'long', year:'numeric'};
 document.getElementById('lastModified').textContent = new Date().toLocaleDateString ('en-US', date);
+
+const hamburgerElement =document.querySelector("#myButton");
+const navElement = document.querySelector(".menuLinks");
+
+hamburgerElement.addEventListener("click", ()=>{
+                    navElement.classList.toggle ("open");
+                    hamburgerElement.classList.toggle ("open");
+
+});
