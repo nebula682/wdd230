eea4472446af0bcad150413ff4e27e4ff2357938
nebula682 <malcolmnie@gmail.com>
2024-05-03 17:09:44 +0200

    chamber of commerce

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -1,3 +1,13 @@
+ // Get the current year
+ var currentYear = new Date().getFullYear();
+
+ // Insert the current year into the HTML element
+ document.getElementById('copyrightYear').innerText = currentYear;
+
+
+
+
+
 //toLocaleDateString
 const date = {weekday:'long', day:'numeric',month:'long', year:'numeric'};
 document.getElementById('lastModified').textContent = new Date().toLocaleDateString ('en-US', date);
