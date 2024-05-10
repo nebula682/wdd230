c4ded2844abf684ef58cc480cbb9c00af249d30e
nebula682 <malcolmnie@gmail.com>
2024-05-10 13:56:29 +0200

    week 03 assignments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,8 +6,33 @@ hamburgerElement.addEventListener('click', ()=> {
                     hamburgerElement.classList.toggle('open');
 
 })
-const myButton = document.querySelector("#myBtn")
+const myButton = document.querySelector("#myBtn");
 
 myButton.addEventListener('click', ()=> {
-                    main.classList.toggle("dark");
+                    myButton.classList.toggle("dark")
+                    
 });
+// Retrieve the visit count from local storage
+let visitCount = localStorage.getItem('visitCount');
+
+// Check if visitCount is null or undefined
+if (visitCount === null || visitCount === undefined) {
+    // If it is null or undefined, set it to 0
+    visitCount = 0;
+} else {
+    // If not, parse the count from string to integer
+    visitCount = parseInt(visitCount);
+}
+
+// Increment the visit count
+visitCount++;
+
+// Update the visit count in the HTML
+document.getElementById('visitCount').innerText = visitCount;
+
+// Store the updated count in local storage
+localStorage.setItem('visitCount', visitCount);
+
+
+
+
